0509ab1a6729eec48e650c7e934adecffe9aa527
Ansuman <109610031+Ansuman-07@users.noreply.github.com>
2023-10-02 20:27:04 +0530

    Your commit message here

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import homelogo from './meme-generator.png'
 
 
-function Navbar({ handleSearch,SearchSection }) {
+function Navbar({ handleSearch, SearchSection }) {
   const [nav, setNav] = useState(false);
 
   return (<>
@@ -11,39 +11,39 @@ function Navbar({ handleSearch,SearchSection }) {
 
       <div className="left flex pr-2">
         <div className="logo ml-4 mr-4 flex items-center">
-        <img src={homelogo} alt="Meme-Generator" />
-        <Link to='/about'><span style={{fontSize:25}}>&#10067;</span></Link>
+          <img src={homelogo} alt="Meme-Generator" />
+          <Link to='/about'><span style={{ fontSize: 25 }}>&#10067;</span></Link>
         </div>
       </div>
 
 
       <div className="right flex">
-    {!nav && (
-    <>
-      <div className="home mr-4 hidden md:flex">
-        <Link to="/" className='hover:bg-blue-600 bg-blue-700 duration-150 px-2 py-1 rounded-md'>Home</Link>
-      </div>
-      <div className="edit mr-4 hidden md:flex">
-        <Link to="/customedit" className='hover:bg-blue-600 bg-blue-700 duration-150 px-2 py-1 rounded-md'>Custom Edit</Link>
-      </div>
+        {!nav && (
+          <>
+            <div className="home mr-4 hidden md:flex">
+              <Link to="/" className='hover:bg-blue-600 bg-blue-700 duration-150 px-2 py-1 rounded-md'>Home</Link>
+            </div>
+            <div className="edit mr-4 hidden md:flex">
+              <Link to="/customedit" className='hover:bg-blue-600 bg-blue-700 duration-150 px-2 py-1 rounded-md'>Custom Edit</Link>
+            </div>
 
-      {SearchSection && (
-        <>
-          <input
-            type="text"
-            placeholder="Search..."
-            id="search-bar"
-            className="hidden md:flex px-2 py-1 text-gray-800 rounded-md border border-gray-300 focus:outline-none focus:border-blue-400"
-          />
-          <button className="hidden md:flex ml-2 mr-6 bg-blue-700 duration-150 text-white py-1 px-3 rounded-md hover:bg-blue-600" onClick={handleSearch}>
-            Search
-          </button>
-        </>
-      )}
-    </>
-  )}
-  <button className='px-2 py-1 mr-4 md:hidden flex' onClick={() => { if (nav) { setNav(false) } else { setNav(true) } }}>MENU</button>
-</div>
+            {SearchSection && (
+              <>
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  id="search-bar"
+                  className="hidden md:flex px-2 py-1 text-gray-800 rounded-md border border-gray-300 focus:outline-none focus:border-blue-400"
+                />
+                <button className="hidden md:flex ml-2 mr-6 bg-blue-700 duration-150 text-white py-1 px-3 rounded-md hover:bg-blue-600" onClick={handleSearch}>
+                  Search
+                </button>
+              </>
+            )}
+          </>
+        )}
+        <button className='px-2 py-1 mr-4 md:hidden flex' onClick={() => { if (nav) { setNav(false) } else { setNav(true) } }}>MENU</button>
+      </div>
 
 
     </nav>
@@ -55,18 +55,18 @@ function Navbar({ handleSearch,SearchSection }) {
         <Link to="/customedit" className='hover:border-2 px-2 py-1 rounded-md'>Custom Edit</Link>
       </div>
       {SearchSection && (
-      <div className="search flex">
-        <input
-          type="text"
-          placeholder="Search..."
-          id="search-bar"
-          className="mt-2 px-2 py-1 text-gray-800 rounded-md border border-gray-300 focus:outline-none focus:border-blue-400"
-        />
-        <button className=" ml-2 mr-4 bg-blue-700 text-white py-1 px-3 rounded-md hover:bg-blue-600 " onClick={handleSearch}>
-          Search
-        </button>
+        <div className="search flex">
+          <input
+            type="text"
+            placeholder="Search..."
+            id="search-bar"
+            className="mt-2 px-2 py-1 text-gray-800 rounded-md border border-gray-300 focus:outline-none focus:border-blue-400"
+          />
+          <button className=" ml-2 mr-4 bg-blue-700 text-white py-1 px-3 rounded-md hover:bg-blue-600 " onClick={handleSearch}>
+            Search
+          </button>
 
-      </div>)}
+        </div>)}
 
     </div>)}
   </>
